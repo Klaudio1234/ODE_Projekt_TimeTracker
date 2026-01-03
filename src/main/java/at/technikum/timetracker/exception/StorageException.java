25fde0786c303810e3770066b84622fc4c04ca4f
Klevis Gjergjaj <gjergjajklevis@gmail.com>
2026-01-03 15:27:44 +0100

    StorageException Class added

--- src/main/java/at/technikum/timetracker/exception/StorageException.java
+++ src/main/java/at/technikum/timetracker/exception/StorageException.java
@@ -1,4 +1,7 @@
 package at.technikum.timetracker.exception;
 
-public class StorageException {
+public class StorageException extends Exception {
+
+    public StorageException(String message) { super(message); }
+    public StorageException(String message, Throwable cause) { super(message, cause); }
 }
