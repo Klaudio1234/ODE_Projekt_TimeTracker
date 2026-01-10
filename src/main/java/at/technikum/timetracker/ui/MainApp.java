7f6d517164419624c404b7956afccfa7a5572ab6
Klevis Gjergjaj <gjergjajklevis@gmail.com>
2026-01-10 14:05:00 +0100

    MainApp programmed

--- src/main/java/at/technikum/timetracker/ui/MainApp.java
+++ src/main/java/at/technikum/timetracker/ui/MainApp.java
@@ -1,4 +1,22 @@
 package at.technikum.timetracker.ui;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        MainController controller = new MainController();
+        Scene scene = new Scene(controller.getRoot(), 1100, 650);
+        stage.setTitle("ODE TimeTracker");
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(e -> controller.shutdown());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
