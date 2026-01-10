9ec3d576b4cdc7cf1420d9ab718fe9d1ff8e1a2e
3r1n6 <erindvora@gmail.com>
2026-01-10 13:41:22 +0100

    MainController base program added

--- src/main/java/at/technikum/timetracker/ui/MainController.java
+++ src/main/java/at/technikum/timetracker/ui/MainController.java
@@ -1,4 +1,19 @@
 package at.technikum.timetracker.ui;
 
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
+
+
+    private final BorderPane root = new BorderPane();
+
+    public MainController() {
+
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
 }
+
